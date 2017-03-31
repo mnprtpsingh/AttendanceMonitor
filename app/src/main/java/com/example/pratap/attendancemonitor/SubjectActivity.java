50962e167172f2de781f575d908d311c51d51bfb
mnprtpsingh <mnprtpsingh@gmail.com>
2017-04-01 00:18:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pratap/attendancemonitor/SubjectActivity.java
@@ -0,0 +1,13 @@
+package com.example.pratap.attendancemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+    }
+}
