14f066477768fe7ab1341a898f674618f6032a47
mnprtpsingh <mnprtpsingh@gmail.com>
2017-04-02 13:20:39 +0530

    Implement Update Subject Feature

--- app/src/main/java/com/example/pratap/attendancemonitor/MonitorCursorAdapter.java
+++ app/src/main/java/com/example/pratap/attendancemonitor/MonitorCursorAdapter.java
@@ -4,12 +4,16 @@ package com.example.pratap.attendancemonitor;
  * Created by pratap on 1/4/17.
  */
 
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.pratap.attendancemonitor.data.SubjectContract.SubjectEntry;
@@ -21,14 +25,19 @@ import com.example.pratap.attendancemonitor.data.SubjectContract.SubjectEntry;
  */
 public class MonitorCursorAdapter extends CursorAdapter {
 
+    private Context mContext;
     /**
      * Constructs a new {@link MonitorCursorAdapter}.
      *
      * @param context The context
      * @param c       The cursor from which to get the data.
+     *
+     *
      */
     public MonitorCursorAdapter(Context context, Cursor c) {
+
         super(context, c, 0 /* flags */);
+        mContext = context;
     }
 
     /**
@@ -57,20 +66,22 @@ public class MonitorCursorAdapter extends CursorAdapter {
      *                correct row.
      */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.tv_name_lable);
         TextView presentTextView = (TextView) view.findViewById(R.id.tv_present);
         TextView absentTextView = (TextView) view.findViewById(R.id.tv_absent);
         TextView percentTextView = (TextView) view.findViewById(R.id.tv_percent_attendance);
-
+        ImageView editImageView = (ImageView) view.findViewById(R.id.icon_edit);
 
         // Find the columns of subject attributes that we're interested in
+        final int idColumnIndex = cursor.getColumnIndex(SubjectEntry._ID);
         int nameColumnIndex = cursor.getColumnIndex(SubjectEntry.COLUMN_SUBJECT_NAME);
         int presentColumnIndex = cursor.getColumnIndex(SubjectEntry.COLUMN_NUMBER_OF_DAYS_PRESENT);
         int absentColumnIndex = cursor.getColumnIndex(SubjectEntry.COLUMN_NUMBER_OF_DAYS_ABSENT);
 
         // Read the subject attributes from the Cursor for the current subject
+        final long id = cursor.getLong(idColumnIndex);
         String Name = cursor.getString(nameColumnIndex);
         int present = cursor.getInt(presentColumnIndex);
         int absent = cursor.getInt(absentColumnIndex);
@@ -81,5 +92,25 @@ public class MonitorCursorAdapter extends CursorAdapter {
         absentTextView.setText(String.valueOf(absent));
         String percentAttendence = " % Attendance";
         percentTextView.setText(percentAttendence);
+        editImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create new intent to go to {@link SubjectActivity}
+                Intent intent = new Intent(mContext, SubjectActivity.class);
+
+                // Form the content URI that represents the specific pet that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link SubjectEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.pratap.attendancemonitor/subjects/2"
+                // if the pet with ID 2 was clicked on.
+                Uri currentSubjectUri = ContentUris.withAppendedId(SubjectEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentSubjectUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
